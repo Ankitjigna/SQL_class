96ff2dcf6032ffa373075421b2c27ba10983fa6e
Ankitjigna <ankitjigna01@gmail.com>
2024-09-18 14:54:29 +0530

    using sql

---
+++ index.js
@@ -0,0 +1,32 @@
+const { faker } = require('@faker-js/faker');
+const mysql = require("mysql2");
+
+const connection =  mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'practice',
+    password: 'Ankit1@#$'
+  });
+  
+try {
+    connection.query("SHOW TABLES",(err,result)=>{
+        if(err) throw err;
+        console.log(result);            
+    })
+} catch (err) {
+    console.log(err);
+}
+
+
+let getUser = ()=>{
+    return{
+        id : faker.string.uuid(),
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+    }
+} ;
+
+console.log(getUser());
+
+
